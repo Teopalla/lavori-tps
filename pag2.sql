5c93b4c4eab011b9c4398c4d297de258916f5a73
Teopalla <119901059+Teopalla@users.noreply.github.com>
2025-01-31 10:26:28 +0100

    Add files via upload

---
+++ pag2.sql
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<anagrafica>
+ <dati>
+ <persona>
+ <nome>Matteo</nome>
+ <cognome>Pallavici</cognome>
+<indirizzo>via Santorre di santa rosa 1</indirizzo>
+<citta>rho</citta>
+<cap>20017</cap>
+<cellulare>3925788743</cellulare>
+ </persona>
+ </dati>
+ <dati>
+ <persona>
+ <nome>Pietro</nome>
+ <cognome>Ottana</cognome>
+<indirizzo>viale ceccarini 23</indirizzo>
+<citta>riccione</citta>
+<cap>47838</cap>
+<cellulare>3481046969</cellulare>
+ </persona>
+ </dati>
+ <dati>
+ <persona>
+ <nome>Lionel</nome>
+ <cognome>Messi</cognome>
+<indirizzo>via roma 54</indirizzo>
+<citta>belluno</citta>
+<cap>32100</cap>
+<cellulare>3314326780</cellulare>
+ </persona>
+ </dati>
+ <dati>
+ <persona>
+ <nome>Tommaso</nome>
+ <cognome>Dimaledetto</cognome>
+<indirizzo>via raffaele sanzio 1</indirizzo>
+<citta>ostuni</citta>
+<cap>72017</cap>
+<cellulare>339 6547112</cellulare>
+ </persona>
+ </dati>
+ </anagrafica>
